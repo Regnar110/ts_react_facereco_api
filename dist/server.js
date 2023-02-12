208bc21a50ea7463cf808780a4c618c2b2e8662f
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-02-13 00:59:14 +0100

    Added Knex and PG packages. Implemented conection sever<==>PSQL database.

--- dist/server.js
+++ dist/server.js
@@ -29,97 +29,80 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importStar(require("express"));
 const bcrypt_1 = __importDefault(require("bcrypt"));
 const cors_1 = __importDefault(require("cors"));
+const knex_1 = require("knex");
 const dotenv = __importStar(require("dotenv"));
 dotenv.config();
 const faceRecognition_1 = require("./utils/faceRecognition");
-const app = (0, express_1.default)();
-let database = {
-    users: [
-        {
-            id: "123",
-            name: "John",
-            email: "john@gmail.com",
-            password: bcrypt_1.default.hashSync("cookies", 10),
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: "124",
-            name: "Mati",
-            email: "Mati@gmail.com",
-            password: bcrypt_1.default.hashSync("ciastko", 10),
-            entries: 1,
-            joined: new Date()
-        },
-        {
-            id: "125",
-            name: "Jan",
-            email: "jan@gmail.com",
-            password: bcrypt_1.default.hashSync("arbuz", 10),
-            entries: 3,
-            joined: new Date()
-        },
-        {
-            id: "126",
-            name: "Kamil",
-            email: "kamil@gmail.com",
-            password: bcrypt_1.default.hashSync("raples", 10),
-            entries: 10,
-            joined: new Date()
-        }
-    ]
+const config = {
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: 'postgres',
+        password: '587188Ab',
+        database: 'smartbrain'
+    }
 };
+const db = (0, knex_1.knex)(config); // knex INIT
+const app = (0, express_1.default)();
 app.use((0, express_1.json)());
 app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
-    res.json(database.users);
+    res.json('get connection');
 });
 app.post("/signin", (req, res) => {
-    const body = req.body;
-    console.log(database);
-    const matchedUser = database.users.filter(user => {
-        const passwordMatched = bcrypt_1.default.compareSync(body.password, user.password);
-        return body.email === user.email && passwordMatched;
-    });
-    if (matchedUser.length > 0) {
-        const userClone = structuredClone(matchedUser[0]);
-        //używamy structuredClone by nie ingierować metodą delete w oryginalny obiekt w pamięci.
-        delete userClone.password;
-        console.log(matchedUser);
-        res.status(200).json(userClone);
-    }
-    else {
-        res.status(400).json("user not found");
-    }
+    const { password, email } = req.body;
+    db.select('email', 'has').from('login')
+        .where({ email: email })
+        .then(data => {
+        const passwordMatched = bcrypt_1.default.compareSync(password, data[0].has);
+        if (passwordMatched) {
+            return db.select('*').from('users')
+                .where('email', email)
+                .then(user => {
+                res.json(user[0]);
+            })
+                .catch(err => res.json('sign in passwordMatch db select error'));
+        }
+        else {
+            res.json("wron email or password");
+        }
+    }).catch(err => res.json('/signin route db select error'));
 });
 app.post("/register", (req, res) => {
-    const body = req.body;
-    const hashedPass = bcrypt_1.default.hashSync(body.password, 10);
-    database.users.push({
-        id: "127",
-        name: body.name,
-        email: body.email,
-        password: hashedPass,
-        entries: 0,
-        joined: new Date()
+    const { name, email, password } = req.body;
+    const hashedPass = bcrypt_1.default.hashSync(password, 10);
+    db.transaction(trx => {
+        trx('login').insert({
+            has: hashedPass,
+            email: email
+        })
+            .returning('email')
+            .then(loginEmail => {
+            trx('users').returning('*').insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date()
+            }).then(user => {
+                res.json(user[0]);
+            }).catch(err => res.json('Unable to register'));
+        })
+            .then(trx.commit)
+            .catch(err => trx.rollback);
     });
-    const userClone = structuredClone(database.users[database.users.length - 1]);
-    delete userClone.password;
-    res.json(userClone);
 });
 app.get('/profile/:id', (req, res) => {
-    const { body } = req;
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+    db.select('*').from('users').where('id', id).then(user => {
+        if (user.length) {
+            res.json(user[0]);
         }
+        else {
+            res.status(400).json("not found user");
+        }
+    }).catch(err => {
+        res.status(400).json("Error getting user");
     });
-    if (!found) {
-        res.status(400).json("Not Found");
-    }
 });
 app.put("/image", async (req, res) => {
     const { id, imageURL } = req.body;
@@ -128,13 +111,9 @@ app.put("/image", async (req, res) => {
         res.json("There is no faces on image");
     }
     else {
-        for (let el of database.users) {
-            if (el.id === id) {
-                el.entries++;
-                let entries = el.entries;
-                res.json({ entries, fr_response });
-            }
-        }
+        db('users').where('id', '=', id).increment('entries', 1).returning('entries').then(entries => {
+            res.json({ entries: entries[0].entries, fr_response });
+        });
     }
 });
 app.listen(3001, () => console.log(`running on 3001`));
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,23 @@ import express, {json, Request, Response, Express, NextFunction } from "express"
 import { RegisterReq, SignInReq } from "./Interfaces/request_inerfaces";
 import bcrypt from "bcrypt"
 import cors from "cors"
+import { Knex, knex } from 'knex'
 import * as dotenv from 'dotenv'
 dotenv.config()
 import { faceRecognition } from "./utils/faceRecognition";
+
+const config:Knex.Config = {
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : '587188Ab',
+      database : 'smartbrain'
+    }
+}
+
+const db = knex(config) // knex INIT
 
 const app:Express = express();
 interface USER_DATA {
@@ -16,98 +30,68 @@ interface USER_DATA {
     joined: Date
 }
 
-let database = {
-    users: [
-        {
-            id: "123",
-            name: "John",
-            email: "john@gmail.com",
-            password: bcrypt.hashSync("cookies", 10),
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: "124",
-            name: "Mati",
-            email: "Mati@gmail.com",
-            password: bcrypt.hashSync("ciastko", 10),
-            entries: 1,
-            joined: new Date()
-        },
-        {
-            id: "125",
-            name: "Jan",
-            email: "jan@gmail.com",
-            password: bcrypt.hashSync("arbuz", 10),
-            entries: 3,
-            joined: new Date()
-        },
-        {
-            id: "126",
-            name: "Kamil",
-            email: "kamil@gmail.com",
-            password: bcrypt.hashSync("raples", 10),
-            entries: 10,
-            joined: new Date()
-        }
-    ]
-}
-
 app.use(json());
 app.use(cors())
 
 app.get("/", (req, res) => {
-    res.json(database.users)
+    res.json('get connection')
 })
 
 
 app.post("/signin", (req,res) => {
-    const body:SignInReq = req.body
-    console.log(database)
-    const matchedUser:USER_DATA[] = database.users.filter(user =>{
-        const passwordMatched = bcrypt.compareSync(body.password, user.password);
-        return body.email === user.email && passwordMatched     
-    })
-    if(matchedUser.length > 0) {
-        const userClone = structuredClone(matchedUser[0]);
-        //używamy structuredClone by nie ingierować metodą delete w oryginalny obiekt w pamięci.
-        delete userClone.password
-        console.log(matchedUser)
-        res.status(200).json(userClone)
-    } else {
-        res.status(400).json("user not found")
-    }
+    const {password, email}:SignInReq = req.body
+    db.select('email', 'has').from('login')
+    .where({email: email})
+    .then(data => {
+        const passwordMatched = bcrypt.compareSync(password, data[0].has);
+        if(passwordMatched) {
+            return db.select('*').from('users')
+            .where('email', email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.json('sign in passwordMatch db select error'))
+        } else {
+           res.json("wron email or password") 
+        }
+    }).catch(err => res.json('/signin route db select error'))
 })
 
 app.post("/register", (req,res) => {
-    const body:RegisterReq = req.body
-    const hashedPass = bcrypt.hashSync(body.password, 10)
-    database.users.push({
-        id:"127",
-        name: body.name,
-        email: body.email,
-        password: hashedPass,
-        entries: 0,
-        joined: new Date()
+    const {name, email, password}:RegisterReq = req.body
+    const hashedPass = bcrypt.hashSync(password, 10)
+    db.transaction(trx => {
+        trx('login').insert({
+            has: hashedPass,
+            email: email
+        })
+        .returning('email')
+        .then(loginEmail => {
+            trx('users').returning('*').insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date()
+            }).then(user => {
+                res.json(user[0])
+            }).catch(err => res.json('Unable to register'))
+        })
+        .then(trx.commit)
+        .catch(err => trx.rollback)
     })
-    const userClone:USER_DATA = structuredClone(database.users[database.users.length-1])
-    delete userClone.password
-    res.json(userClone)
 })
-
 app.get('/profile/:id', (req, res) => {
-    const {body} = req;
     const {id} = req.params
-    let found = false
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true
-            return res.json(user)
+    db.select('*').from('users').where('id', id).then(user => {
+        if(user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json("not found user")
         }
+        
+    }).catch(err => {
+        res.status(400).json("Error getting user")
     })
-    if(!found){
-        res.status(400).json("Not Found")
-    }
+
 })
 
 app.put("/image", async (req, res) => {
@@ -116,13 +100,9 @@ app.put("/image", async (req, res) => {
     if(typeof fr_response === "boolean") {
         res.json("There is no faces on image")
     } else {
-        for(let el of database.users) {
-            if(el.id === id) {
-                el.entries++
-                let entries = el.entries
-                res.json({entries, fr_response})
-            }
-        }
+        db('users').where('id', '=', id).increment('entries', 1).returning('entries').then(entries => {
+            res.json({entries:entries[0].entries, fr_response})
+        })
     }
     
     
